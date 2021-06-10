5d3feef7bd04c411398dffdf842c2398e3a2b5c5
Samrhan <samuel.bader83@gmail.com>
2021-06-10 15:10:47 +0200

    Initial commit

---
+++ pharmacist/main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('dist/pharmacist/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
