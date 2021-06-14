b4f096c779f5d81bfe7b5d1e6717155503f47202
Shasless <54817015+Shasless@users.noreply.github.com>
2021-06-14 12:57:01 +0200

    /login /disconnect et /me

---
+++ server/server/routes/disconnect.js
@@ -0,0 +1,6 @@
+module.exports = async (req, res, client) => {
+    req.session.destroy((err) => {
+        res.status(200).send("OK");
+    });
+
+}
--- server/server/routes/login.js
+++ server/server/routes/login.js
@@ -1,4 +1,55 @@
+const bcrypt = require('bcrypt')
+
 module.exports = async (req, res, client) => {
-    let data = await client.query("SELECT * FROM medecins_complet")
-    res.send(data)
+
+    const username = req.body.username;
+    const rpps  = req.body.rpps;
+    const password = req.body.password
+
+    if (req.session.userId) {
+        res.status(403).json({message: "already logged in"})
+        return;
+    }
+    if(!((username || rpps )&& password)){
+        res.status(400).json({message: "bad request - request must include username or rpps and password"});
+        return;
+    }
+    let data;
+    if(username){
+        data = await client.query("SELECT * FROM pharmaciens WHERE username = ?",[username])
+    }else if(rpps){
+        data = await client.query("SELECT * FROM medecins WHERE rpps = ?",[rpps])
+    }else{
+        res.status(400).json({message: "bad request - request must include username or rpps and password"});
+        return;
+    }
+
+    var result = Object.values(JSON.parse(JSON.stringify(data[0])))[0]
+
+    console.log(result)
+    console.log(result.Password)
+    console.log(data[0].length)
+
+    if (data[0].length === 1) {
+
+        if (await bcrypt.compare(password, result.Password)) {
+            console.log("aaa")
+
+            if(username){
+                console.log("cccc")
+                req.session.userId = result.Id_pharmacien;
+                req.session.TypeID = 0;
+            }else if(rpps){
+                req.session.userId = result.Id_medecin;
+                req.session.TypeID = 1;
+            }
+            res.status(200).json({message: "ok", id: req.session.userId})
+        } else {
+            res.status(400).json({message: "bad request"});
+        }
+}else{
+        res.status(400).json({message: "bad request"});
+
+    }
+
 }
