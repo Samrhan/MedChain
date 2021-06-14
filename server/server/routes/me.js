2804a062654d7c8dfd537f5fbfec96fe799ade7b
Shasless <54817015+Shasless@users.noreply.github.com>
2021-06-14 12:57:01 +0200

    /login /disconnect et /me

---
+++ server/server/routes/me.js
@@ -0,0 +1,25 @@
+module.exports = async (req, res, client) => {
+    if(req.session.userId){
+        let sql;
+        if(req.session.TypeID === 0){
+            sql = "SELECT username, nom_pharmacien, prenom_pharmacien FROM pharmaciens WHERE id_pharmacien=?"
+        }else{
+            sql = "SELECT RPPS, nom_medecin, prenom_medecin FROM medecins WHERE id_medecin=?"
+        }
+        let data = await client.query(sql,[req.session.userId])
+        var result = Object.values(JSON.parse(JSON.stringify(data[0])))[0]
+
+        if(result){
+            if(req.session.TypeID === 0){
+                result.role = "pharmacien"
+            }else{
+                result.role = "medecin"
+            }
+            res.status(200).json(result);
+        } else {
+            res.status(500).json({message: 'internal server error'});
+        }
+    } else {
+        res.status(401).json({message: "no user logged in."});
+    }
+}
