28d947b94362b19f6061901081496cc6fd0aa63d
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-16 17:33:45 +0200

    Routage après login + changement des codes d'erreur HTTP + ajout de la variable user_type à la requête de login

--- pharmacist/src/app/app.module.ts
+++ pharmacist/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginPageComponent } from './login-page/login-page.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { ScanOrdonnanceComponent } from './scan-ordonnance/scan-ordonnance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    ScanOrdonnanceComponent
   ],
     imports: [
         BrowserModule,
