d03c5da576436ad77b7add4cf2c4616d3d686ede
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-16 17:07:18 +0200

    Page de connexion fonctionnelle

--- pharmacist/src/app/app-routing.module.ts
+++ pharmacist/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from "./login-page/login-page.component";
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '**', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
