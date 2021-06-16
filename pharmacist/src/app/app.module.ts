45e4a84c4a8c473d0d53caefe22ae66838fc82e8
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-16 15:24:30 +0200

    Ajout de bootstrap + création du composant "login-page"

--- pharmacist/src/app/app.module.ts
+++ pharmacist/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginPageComponent } from './login-page/login-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
