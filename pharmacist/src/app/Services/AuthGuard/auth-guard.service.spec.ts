4920ce444a76a8af8fc8e3f5254209bc62aaf640
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-17 18:13:20 +0200

    Tests unitaires sur les services

--- pharmacist/src/app/Services/AuthGuard/auth-guard.service.spec.ts
+++ pharmacist/src/app/Services/AuthGuard/auth-guard.service.spec.ts
@@ -4,13 +4,15 @@ import { AuthGuardService } from './auth-guard.service';
 import {UserService} from "../User/user.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
+import {of, throwError} from "rxjs";
 
 describe('AuthGuardService', () => {
   let service: AuthGuardService;
+  let mockUserService = jasmine.createSpyObj(['getConnectedUser']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService],
+      providers: [{provide: UserService, useValue: mockUserService}],
       imports: [HttpClientTestingModule, RouterTestingModule]
     });
     service = TestBed.inject(AuthGuardService);
@@ -19,4 +21,16 @@ describe('AuthGuardService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should throw error when no user is connected', () => {
+    mockUserService.getConnectedUser.and.returnValue(throwError({status: 401}))
+
+    expect(service.canActivate).toThrowError();
+  })
+
+  it('should return true when a user is connected', () => {
+    mockUserService.getConnectedUser.and.returnValue(of({role: "pharmacien"}))
+
+    expect(service.canActivate()).toBeTruthy();
+  })
 });
