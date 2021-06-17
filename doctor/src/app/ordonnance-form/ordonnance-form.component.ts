d712ef9c6bef9c53152b862808c09c5a713158bd
Samrhan <samuel.bader@efrei.net>
2021-06-17 21:37:10 +0200

    Auth guard and prevent services

---
+++ doctor/src/app/ordonnance-form/ordonnance-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordonnance-form',
+  templateUrl: './ordonnance-form.component.html',
+  styleUrls: ['./ordonnance-form.component.css']
+})
+export class OrdonnanceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
