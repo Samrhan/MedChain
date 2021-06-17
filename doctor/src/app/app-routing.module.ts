d6f25f7ad9b58d636e803fe192b673119b55d7ee
Samrhan <samuel.bader@efrei.net>
2021-06-17 21:37:10 +0200

    Auth guard and prevent services

--- doctor/src/app/app-routing.module.ts
+++ doctor/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {AuthGuardService} from "./service/auth-guard.service";
 import {AuthPreventService} from "./service/auth-prevent.service";
 import {OrdonnanceFormComponent} from "./ordonnance-form/ordonnance-form.component";
 
 const routes: Routes = [
-  {path: "login", component: LoginComponent, canActivate: [AuthPreventService]},
-  {path: "form_ordonnance", component: OrdonnanceFormComponent, canActivate: [AuthGuardService]},
-  {path: '**', component: LoginComponent, canActivate: [AuthPreventService]}
+  {path: "login", component:LoginComponent, canActivate:[AuthPreventService]},
+  {path: "form_ordonnance", component:OrdonnanceFormComponent, canActivate:[AuthGuardService]},
+  { path: '**', component: LoginComponent, canActivate: [AuthPreventService] }
 
 ];
 
@@ -16,5 +16,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
