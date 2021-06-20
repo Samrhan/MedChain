90b06ed76731acf5aaa92114f089619a07b3c990
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-20 17:03:54 +0200

    Affichage de l'ordonnance

--- pharmacist/src/app/display-ordonnance/display-ordonnance.component.ts
+++ pharmacist/src/app/display-ordonnance/display-ordonnance.component.ts
@@ -7,7 +7,11 @@ import {PrescriptionManagerService} from "../Services/PrescriptionManager/prescr
   styleUrls: ['./display-ordonnance.component.css']
 })
 export class DisplayOrdonnanceComponent implements OnInit {
+  info_ordonnance: any;
+  notes: any;
   prescription: any;
+
+  uses_left: number | undefined;
 
   constructor(
     private prescriptionManagerService: PrescriptionManagerService,
@@ -15,7 +19,15 @@ export class DisplayOrdonnanceComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.prescription = this.prescriptionManagerService.get_prescription_cache();
-    console.log(this.prescription)
+    let prescription = this.prescriptionManagerService.get_prescription_cache();
+    this.prescriptionManagerService.get_uses_left().subscribe(val => this.uses_left = val);
+    this.info_ordonnance = prescription.info_ordonnance;
+    this.notes = prescription.notes;
+    this.prescription = prescription.prescription;
+  }
+
+  formatDate(text: string): string {
+    let date = new Date(text);
+    return date.toLocaleDateString('fr-FR');
   }
 }
