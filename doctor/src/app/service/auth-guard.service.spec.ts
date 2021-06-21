6c95ccef336e64bb4d1df5580be86301cab92909
Samrhan <samuel.bader83@gmail.com>
2021-06-21 13:53:15 +0200

    Add tests

--- doctor/src/app/service/auth-guard.service.spec.ts
+++ doctor/src/app/service/auth-guard.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuardService } from './auth-guard.service';
+import {UserService} from "./user.service";
+import {Router} from "@angular/router";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('AuthGuardService', () => {
   let service: AuthGuardService;
+  let mockUserService = jasmine.createSpyObj(['getConnectedUser']);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{provide: UserService, useValue: mockUserService}],
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    });
     service = TestBed.inject(AuthGuardService);
+    spyOn(service.router, 'navigateByUrl');
   });
 
   it('should be created', () => {
