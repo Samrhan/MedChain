6077e70ec675b448372bc012d82db0468d501234
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-22 17:08:34 +0200

    Tests unitaires navbar.component

--- pharmacist/src/app/navbar/navbar.component.spec.ts
+++ pharmacist/src/app/navbar/navbar.component.spec.ts
@@ -9,10 +9,17 @@ describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
+  let mockAuthenticatorService = jasmine.createSpyObj(['disconnect']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [ AuthenticatorService ],
-      imports: [ HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: AuthenticatorService, useValue: mockAuthenticatorService}
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
@@ -27,4 +34,43 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('disconnect() should call AuthenticatorService methode', () => {
+    component.disconnect();
+
+    expect(mockAuthenticatorService.disconnect).toHaveBeenCalled()
+  })
+
+  it('goBack() should call router with the provided route', () => {
+    spyOn(component.router, 'navigate');
+
+    const route = "/foo"
+    component.go_back_route = route;
+    component.goBack();
+
+    expect(component.router.navigate).toHaveBeenCalledWith([route]);
+  })
+
+  it('should not display the go back button if told not to', (done: DoneFn) => {
+    component.display_goBack_button = false;
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelector('#goBackButton')).toBeFalsy();
+      expect(fixture.nativeElement.querySelector('#fillerButton')).toBeTruthy();
+      done();
+    })
+  })
+
+  it('should display the go back button if told to', (done: DoneFn) => {
+    component.display_goBack_button = true;
+    component.go_back_route = "/foo"
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelector('#goBackButton')).toBeTruthy();
+      expect(fixture.nativeElement.querySelector('#fillerButton')).toBeFalsy();
+      done();
+    })
+  })
+
+
 });
