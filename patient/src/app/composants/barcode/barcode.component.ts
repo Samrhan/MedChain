e4c5ff4ca73b77005db3d7568bfec112fd3a85f5
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-25 18:54:03 +0200

    Affichage de la liste des ordonnances + passage d'une ordonnance à l'autre

--- patient/src/app/composants/barcode/barcode.component.ts
+++ patient/src/app/composants/barcode/barcode.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 import * as bwipjs from 'bwip-js';
 
@@ -7,7 +7,7 @@ import * as bwipjs from 'bwip-js';
   templateUrl: './barcode.component.html',
   styleUrls: ['./barcode.component.css']
 })
-export class BarcodeComponent implements OnInit {
+export class BarcodeComponent implements OnInit, OnChanges {
 
   @Input() code: string = '';
   @Input() password: string = '';
@@ -17,6 +17,14 @@ export class BarcodeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.drawBarcode()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.drawBarcode()
+  }
+
+  drawBarcode(): void {
     let canvas = bwipjs.toCanvas('barcode-canvas', {
       bcid:        'datamatrix',       // Barcode type
       text:        this.code + "\n" + this.password,    // Text to encode
--- patient/src/app/composants/main-page/main-page.component.ts
+++ patient/src/app/composants/main-page/main-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {PrescriptionsManagerService} from "../../services/prescriptionManager/prescriptions-manager.service";
 
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-main-page',
@@ -11,12 +13,67 @@ import { faCog } from "@fortawesome/free-solid-svg-icons";
 export class MainPageComponent implements OnInit {
 
   faCog = faCog;
+  faArrowLeft = faArrowLeft;
+  faArrowRight = faArrowRight;
+
+  prescriptions: Array<any>;
+  max_value: number = 0;
+  current_prescription: number = 0;
 
   constructor(
     public prescriptionManager: PrescriptionsManagerService
-  ) { }
+  ) {
+    // TODO: Remove this
+    prescriptionManager.clearAll();
+    prescriptionManager.addPrescription("d92984f1-d19f-4bdc-926b-6e81e8ce75d5", "b07174a9-3c48-4aa4-9f03-b87f385de469");
+    prescriptionManager.addPrescription("755da544-56f4-466c-a454-f1bc23236cf3", "08a99940-e030-4e97-a865-07c17364c9c6");
+    prescriptionManager.addPrescription("81732cb4-e652-4ad2-8d2b-5878062a3a5f", "64308ae0-fdbf-49f4-9333-57d10a765219");
+
+    this.prescriptions = prescriptionManager.getAllPrescriptions();
+    this.max_value = this.prescriptions.length - 1;
+  }
 
   ngOnInit(): void {
+  }
+
+  next_prescription(){
+    this.current_prescription = Math.min(this.current_prescription + 1, this.max_value);
+  }
+
+  prev_prescription(){
+    this.current_prescription = Math.max(this.current_prescription - 1, 0);
+  }
+
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end' && this.swipeTime && this.swipeCoord) {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30 // Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
+        switch (swipe){
+          case 'next': {
+            this.next_prescription();
+            break;
+          }
+          case 'previous': {
+            this.prev_prescription();
+            break;
+          }
+        }
+      }
+    }
   }
 
 }
